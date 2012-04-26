8e37e4ca7c13f1277fce3ba54078e6ad6efec85f
Andres Gonzalez M <angonzal@uniandes.edu.co>
2012-04-26 13:59:16 -0500

    Posprocessing rutines for calculating the stress tensor and viscosity contribution of a cell suspended in a shear flow.

--- mdrops/postprovesrheo_trans.m
+++ mdrops/postprovesrheo_trans.m
@@ -5,26 +5,12 @@ clear; clc; sbar = systembar(); raiz = cd;
 % indique los nombres de las carpeta de origen de la soluciones Indique las
 % soluciones en oreden ascendente desde el mas bajo al mas alto
 
-% % Lambda = 6.4
-% carpetaorigen = {'gotaLambda6.4Ca0.05' 'gotaLambda6.4Ca0.1'...
-%      'gotaLambda6.4Ca0.25' 'gotaLambda6.4Ca0.50'...
-%      'gotaLambda6.4Ca0.75' 'gotaLambda6.4Ca1.00' 'gotaLambda6.4Ca1.50'}
-% 
-% % indique las iteraciones de estado estable para cada solucion
-% itss = [695 281 279 270 639 255 984];
+carpetaorigen = 'rbc_lambda20_ca10';
 
-% % Lambda = 3.6
-% carpetaorigen = {'gotaLambda3.6Ca0.05' 'gotaLambda3.6Ca0.1'...
-%      'gotaLambda3.6Ca0.25' 'gotaLambda3.6Ca0.50' 'gotaLambda3.6Ca0.75'...
-%      'gotaLambda3.6Ca1' 'gotaLambda3.6Ca1.5' 'gotaLambda3.6Ca2.00'}
-% 
-% % indique las iteraciones de estado estable para cada solucion
-% itss = [699 1072 1068 1073 1080 1132 1042 2085];
-
-carpetaorigen = {'rbc_lambda20_ca10'};
-
-% indique las iteraciones de estado estable para cada solucion
-itss = [999];
+% indique las iteraciones totales de la simulacion y el intervalo para los
+% calculos
+itmax = 999;
+interval = 50;
 
 % nombre del archivo de origen
 nombreorigen = 'it';
@@ -32,18 +18,17 @@ nombreorigen = 'it';
 % % Ejes [xmin xmax ymin ymax] Stress Tensor
 % ejes1 = [0 10 -0.5 3.0];
 % % Ejes [xmin xmax ymin ymax] Visc Particle
-% ejes2 = [0 10 0 2.5];
 
-contador = 1;
-y1(1) = 0; y2(1) = 0; y3(1) = 0; y4(1) = 0; capilar(1) = 0;
+contador = 0;
+y1 = []; y2 = []; y3 = []; y4 = []; time = [];
 
-for k=1: max(size(carpetaorigen))
+for k = 1:interval:itmax
     
     contador = contador + 1;
     % cargue el archivo de solucion
     direccion = ...
-        [cd  sbar '..' sbar 'data' sbar carpetaorigen{k} sbar ...
-        nombreorigen num2str(itss(k)) '.mat'];
+        [cd  sbar '..' sbar 'data' sbar carpetaorigen sbar ...
+        nombreorigen num2str(k) '.mat'];
     load(direccion);
    
     % calcule el delta de fuerza en la interfase por curvatura
@@ -53,7 +38,7 @@ for k=1: max(size(carpetaorigen))
         velnode,geom.normal,parms.lamda);
     rintegral = inttrapeciomata(geom.dsi,strtensornode)/geom.vol;
     
-    capilar(contador) = parms.ca;
+    time(contador) = geom.tiempo;
     % S11 - S22 (S22 - S33 este caso)  
     y1(contador) = rintegral(2,2)-rintegral(3,3);
     % S22 - S33 (S33 - S11 este caso)
@@ -66,15 +51,15 @@ for k=1: max(size(carpetaorigen))
 end
 
 figure(1);
-Name = ['Stress Tensor for \lambda=' num2str(parms.lamda)];
-plot(capilar,y1,'--k+',capilar,y2,'-.ko',capilar,y3,'-ks',...
-    capilar,y4,':k>','LineWidth',.5,'MarkerEdgeColor','k','MarkerSize',8)
-xlabel('Ca','fontsize',12)
-ylabel('\tau_{ij}','fontsize',12)
-title(Name,'fontsize',12);
+Name = ['Stress Tensor for \lambda=' num2str(parms.lamda)...
+    ' and Ca=' num2str(parms.ca)];
+plot(time,y1,'--k',time,y2,'-.k',time,y3,'-k',time,y4,':k','LineWidth',1)
+xlabel('Time','fontsize',14)
+ylabel('\tau_{ij}','fontsize',14)
+title(Name,'fontsize',14);
 leyenda=legend('\tau_{11}-\tau_{22}','\tau_{22}-\tau_{33}',...
     '\tau_{12}','(\tau_{ii}/{3})-2',2);
-set(leyenda,'fontsize',12)
+set(leyenda,'fontsize',14)
 
 nameydir = [raiz sbar];
 % axis([ejes1]);
@@ -83,20 +68,20 @@ saveas(1,[nameydir 'strtenlamda' num2str(parms.lamda*10)],'eps')
 saveas(1,[nameydir 'strtenlamda' num2str(parms.lamda*10)],'pdf')
         
 % nondimensionless viscosity pg 111 tesis kennedy
-y7 = y3(2:end)./capilar(2:end);
+y7 = y3(1:end)./parms.ca;
 
 figure(2);
-Name = ['Viscosity \lambda=' num2str(parms.lamda)];
-plot(capilar(2:end),y7,'-ko','LineWidth',0.5,'MarkerEdgeColor',...
-    'k','MarkerSize',8)
-xlabel('Ca','fontsize',12)
-ylabel('\mu*=\tau_{12}/ca','fontsize',12)
-set(gca,'fontsize',12)
-title(Name,'fontsize',12);
+Name = ['Viscosity \lambda=' num2str(parms.lamda) ' and Ca=' num2str(parms.ca)];
+plot(time(1:end),y7,'-k','LineWidth',0.5)
+xlabel('Time','fontsize',14)
+ylabel('\mu*=\tau_{12}/ca','fontsize',14)
+set(gca,'fontsize',14)
+title(Name,'fontsize',14);
 grid on;
 
 nameydir = [raiz sbar];
-% axis([ejes2]);
+ejes2 = [0 geom.tiempo 0 2.5];
+axis([ejes2]);
 saveas(2,[nameydir 'visclamda' num2str(parms.lamda*10)],'fig')
 saveas(2,[nameydir 'visclamda' num2str(parms.lamda*10)],'eps')
 saveas(2,[nameydir 'visclamda' num2str(parms.lamda*10)],'pdf')
